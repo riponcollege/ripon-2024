d2480313a2983c5ee655e622589600a43c5e38c3
James Pederson <james@jpederson.com>
2024-11-20 16:46:50 +0000

    Adjust title styles when there's only 1 menu item, and fix line height in tables.

--- library/component/title.php
+++ library/component/title.php
@@ -12,7 +12,7 @@ $menu_count = $menu_obj->count;
 // if it's not empty, lets output it
 if ( !empty( $title ) ) {
 	?>
-<div class="title-container <?php print $theme ?> <?php print $color; print ( $menu_count<5 ? ' columns' : '' ) ?>">
+<div class="title-container <?php print $theme . ' ' . $color . ' ' . ( $menu_count<5 ? 'columns' : '' ) . ' count-' . $menu_count ?>">
 	<div class="wrap">
 		<div class="title">
 			<h1><?php print $title ?></h1>
