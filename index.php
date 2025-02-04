d7e47c3694562af5b5c38d66bbd6f86e14437ab9
James Pederson <james@jpederson.com>
2025-02-04 16:39:27 +0000

    Clean search term to avoid XSS attacks.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ get_header();
 
 			<?php
 			if ( is_search() ) {
-				?><h1 class="post-title">Search Results for <span>'<?php print $_REQUEST["s"]; ?>'</span></h1><?php
+				?><h1 class="post-title">Search Results for <span>'<?php print filter_var( $_REQUEST["s"], FILTER_SANITIZE_STRING); ?>'</span></h1><?php
 			} else {
 				?><h1 class="post-title">Our Blog</h1><?php
 			}
