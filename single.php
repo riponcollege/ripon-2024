bf523e3eb1eafc70d970a550da0da5ec7287efee
James Pederson <james@jpederson.com>
2025-02-12 19:12:59 +0000

    Adjust individual post styles.

--- single.php
+++ single.php
@@ -10,11 +10,24 @@ get_header();
 	<div class="content-wide" role="main">
 		<?php 
 		if ( have_posts() ) :
-			while ( have_posts() ) : the_post(); 
+			while ( have_posts() ) : the_post();
 				?>
 				<h1><?php the_title(); ?></h1>
+				<div class="content-narrow">
+					<div class="post-thumbnail">
+					<?php
+					$featured_caption = get_post( get_post_thumbnail_id() )->post_excerpt;
+					the_post_thumbnail();
+					if ( !empty( $featured_caption ) ) {
+						echo '<p class="wp-caption-text">' . $featured_caption . '</p>';
+					}
+					?>
+				</div>
 				<?php
 				the_content();
+				?>
+				</div>
+				<?php
 			endwhile;
 		endif;
 		?>
