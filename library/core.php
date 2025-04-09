a24021657069f6c9f86f8a48a6b305e259e5cbfb
James Pederson <james@jpederson.com>
2025-04-09 19:16:26 +0000

    Add support for page excerpts.

--- library/core.php
+++ library/core.php
@@ -194,3 +194,10 @@ function fn_search_courses_only($query) {
 }
 add_action( 'pre_get_posts', 'fn_search_courses_only' );
 
+
+
+function my_add_excerpts_to_pages() {
+	add_post_type_support( 'page', 'excerpt' );
+}
+add_action( 'init', 'my_add_excerpts_to_pages' );
+
