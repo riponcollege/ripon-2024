787ffcf53a45f43a5b0f216563a465bbb61846fb
James Pederson <james@jpederson.com>
2025-04-22 16:02:48 +0000

    Add width column for tables.

--- library/component/table.php
+++ library/component/table.php
@@ -1,6 +1,7 @@
 <?php
 
 $title = get_sub_field( 'title' );
+$widths = get_sub_field( 'widths' );
 $columns = get_sub_field( 'columns' );
 
 if ( have_rows( 'row' ) ) :
@@ -16,13 +17,19 @@ if ( have_rows( 'row' ) ) :
         <?php
     }
 
+    foreach ( array( 'one', 'two', 'three', 'four' ) as $col ) :
+        if ( !empty( $widths[$col] ) ) {
+            $widths[$col] = ' width="' . $widths[$col] . '%"';
+        } 
+    endforeach;
+
     while ( have_rows( 'row' ) ) : the_row();
         ?>
             <tr>
-                <td class="label"><?php the_sub_field( 'label' ); ?></td>
-                <td><?php the_sub_field( 'two' ); ?></td>
-                <?php if ( $columns >= 3 ) : ?><td><?php the_sub_field( 'three' ); ?></td><?php endif; ?>
-                <?php if ( $columns == 4 ) : ?><td><?php the_sub_field( 'four' ); ?></td><?php endif; ?>
+                <td class="label"<?php print $widths['one'] ?>><?php the_sub_field( 'label' ); ?></td>
+                <td<?php print $widths['two'] ?>><?php the_sub_field( 'two' ); ?></td>
+                <?php if ( $columns >= 3 ) : ?><td<?php print $widths['three'] ?>><?php the_sub_field( 'three' ); ?></td><?php endif; ?>
+                <?php if ( $columns == 4 ) : ?><td<?php print $widths['four'] ?>><?php the_sub_field( 'four' ); ?></td><?php endif; ?>
             </tr>
         <?php
     endwhile;
